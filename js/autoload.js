cfb77bc8e93e31de3945d6203a2fc5fd12baeea7
wencong.zhang@foxmail.com <wencong.zhang@foxmail.com>
2021-06-11 03:17:02 +0800

    Site updated: 2021-06-11 03:17:02

---
+++ js/autoload.js
@@ -0,0 +1,19 @@
+const live2d_path="https://cdn.jsdelivr.net/gh/stevenjoezhang/live2d-widget@latest/";function loadExternalResource(s,l){return new Promise((e,i)=>{let t;if(l==="css"){t=document.createElement("link");t.rel="stylesheet";t.href=s}else if(l==="js"){t=document.createElement("script");t.src=s}if(t){t.onload=()=>e(s);t.onerror=()=>i(s);document.head.appendChild(t)}})}Promise.all([loadExternalResource(live2d_path+"waifu.css","css"),loadExternalResource(live2d_path+"live2d.min.js","js"),loadExternalResource(live2d_path+"waifu-tips.js","js")]).then(()=>{initWidget({waifuPath:live2d_path+"waifu-tips.json",cdnPath:"https://cdn.jsdelivr.net/gh/fghrsh/live2d_api/"})});console.log(`
+  く__,.ヘヽ.        /  ,ー､ 〉
+           ＼ ', !-─‐-i  /  /´
+           ／｀ｰ'       L/／｀ヽ､
+         /   ／,   /|   ,   ,       ',
+       ｲ   / /-‐/  ｉ  L_ ﾊ ヽ!   i
+        ﾚ ﾍ 7ｲ｀ﾄ   ﾚ'ｧ-ﾄ､!ハ|   |
+          !,/7 '0'     ´0iソ|    |
+          |.从"    _     ,,,, / |./    |
+          ﾚ'| i＞.､,,__  _,.イ /   .i   |
+            ﾚ'| | / k_７_/ﾚ'ヽ,  ﾊ.  |
+              | |/i 〈|/   i  ,.ﾍ |  i  |
+             .|/ /  ｉ：    ﾍ!    ＼  |
+              kヽ>､ﾊ    _,.ﾍ､    /､!
+              !'〈//｀Ｔ´', ＼ ｀'7'ｰr'
+              ﾚ'ヽL__|___i,___,ンﾚ|ノ
+                  ﾄ-,/  |___./
+                  'ｰ'    !_,.:
+`);
